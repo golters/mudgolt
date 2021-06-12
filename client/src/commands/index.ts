3a9b2374407e94e71f744785e9c89b8601956576
Thomas Bowen <6600748+ttbowen@users.noreply.github.com>
2021-06-12 17:18:38 +0100

    Update the command emitter to search for command aliases

--- client/src/commands/index.ts
+++ client/src/commands/index.ts
@@ -2,60 +2,69 @@ import {
   sendEvent, 
 } from "../network"
 import {
-  CHAT_EVENT, 
-  INPUT_EVENT, 
-  LOG_EVENT,
+  CHAT_EVENT, INPUT_EVENT, LOG_EVENT, 
 } from "../../../events"
 import {
   commandEmitter, 
 } from "./emitter"
-import Roll from './roll'
+import Roll from "./roll"
 import {
   MakeRoom, 
 } from "./makeRoom"
 import {
-  Go,
+  Go, 
 } from "./go"
 import {
   MESSAGE_MAX_LENGTH, 
 } from "../../../constants"
 import {
-  LogItem,
+  LogItem, 
 } from "../components/Terminal"
 import {
   Help, 
 } from "./help"
+import Nick from "./username"
 
-export const commandModules = [
-  Roll,
-  MakeRoom,
-  Go,
-  Help,
-]
+export const commandModules = [Roll, MakeRoom, Go, Help, Nick]
+
+function findCommand(nameOrAlias: string) {
+  return commandModules.find(
+    (cmd) =>
+      cmd.command.toLowerCase() === nameOrAlias.toLowerCase() ||
+      cmd.aliases?.map((alias) => alias.toLowerCase()).includes(nameOrAlias),
+  )
+}
+
+function parseArgs(command: string, input: string) {
+  return input
+    .replace(command, "")
+    .trim()
+    .split(" ")
+    .map((arg) => arg.trim())
+    .filter((arg) => arg !== "")
+}
 
 commandEmitter.on(INPUT_EVENT, (input) => {
   if (input.length > MESSAGE_MAX_LENGTH) {
-    const errorItem = LogItem(`Message must not be longer than ${MESSAGE_MAX_LENGTH} characters.`)
+    const errorItem = LogItem(
+      `Message must not be longer than ${MESSAGE_MAX_LENGTH} characters.`,
+    )
     errorItem.classList.toggle("error-message")
 
     commandEmitter.emit(LOG_EVENT, errorItem)
   }
 
-  for (const module of commandModules) {
-    const commandRegex = new RegExp(`^${module.command}(\\b\\s?|$)`, "i")
+  const commandName = input.trim().split(" ")[0]
+  const command = findCommand(commandName)
+  const args = parseArgs(commandName, input)
 
-    if (commandRegex.test(input)) {
-      const args = input.replace(commandRegex, '').split(" ")
-
-      module.callback({
-        args,
-        input, 
-      })
-
-      return
-    }
+  if (command) {
+    command.callback({
+      args,
+      input,
+    })
   }
-  
+
   sendEvent(CHAT_EVENT, input)
 })
 
