63533b5007ca5828025e937dbc61aede8c13a494
Andr3wRiv3rs <andrewarivers@gmail.com>
2021-06-11 20:15:04 -0700

    Fix command regex
    fixes #3

--- client/src/commands/index.ts
+++ client/src/commands/index.ts
@@ -23,8 +23,10 @@ const commandModules = [
 
 commandEmitter.on(INPUT_EVENT, (input) => {
   for (const module of commandModules) {
-    if (new RegExp(`\\b${module.command}\\b\\s?`, "i").test(input)) {
-      const args = input.replace(new RegExp(`\\b${module.command}\\b\\s?`, "i"), '').split(" ")
+    const commandRegex = new RegExp(`^${module.command}\\b\\s?`, "i")
+
+    if (commandRegex.test(input)) {
+      const args = input.replace(commandRegex, '').split(" ")
 
       module.callback({
         args,
