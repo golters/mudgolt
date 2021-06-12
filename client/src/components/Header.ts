82c3f549f10d11f04e17f8a6b2d6923832827240
Andr3wRiv3rs <andrewarivers@gmail.com>
2021-06-12 14:53:53 -0700

    Responsive banner

--- client/src/components/Header.ts
+++ client/src/components/Header.ts
@@ -16,21 +16,21 @@ export const Header = () => {
   const container = document.createElement("header")
   container.id = "header"
 
-  const details = document.createElement("div")
-  details.id = "room-details"
-  container.appendChild(details)
+  const wrapper = document.createElement("div")
+  wrapper.id = "header-wrapper"
+  container.appendChild(wrapper)
+
+  const name = document.createElement("h3")
+  name.id = "room-name"
+  wrapper.appendChild(name)
+
+  const description = document.createElement("p")
+  description.id = "room-description"
+  wrapper.appendChild(description)
 
   const banner = document.createElement("div")
   banner.id = "banner"
-  container.appendChild(banner)
-
-  const resizeHeader = () => {
-    container.style.fontSize = `${window.innerWidth / 1920}em`
-  }
-
-  resizeHeader()
-
-  window.addEventListener("resize", resizeHeader)
+  wrapper.appendChild(banner)
 
   networkEmitter.on(ROOM_UPDATE_EVENT, (room: Room) => {
     const bannerParts: Node[] = []
@@ -47,7 +47,8 @@ export const Header = () => {
 
     banner.append(...bannerParts)
 
-    details.textContent = `${room.name}\n\n${room.description}`
+    name.textContent = room.name
+    description.textContent = room.description
   })
 
   return container
