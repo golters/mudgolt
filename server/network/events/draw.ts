cc473e490ba62273906159726485117b83ed6480
IamtheMa <83910227+IamtheMa@users.noreply.github.com>
2021-07-19 10:43:08 +0100

    Add files via upload
    draw event

---
+++ server/network/events/draw.ts
@@ -0,0 +1,34 @@
+import {
+	NetworkEventHandler,
+	networkEmitter,
+} from "./emitter"
+import {
+	online,
+	sendEvent,
+	broadcastToRoom,
+} from "../"
+import {
+	DRAW_EVENT,
+	ERROR_EVENT,
+} from "../../../events"
+import {
+	getRoomById,
+	editBaner,
+} from "../../services/room"
+import {
+	Room
+} from "@types"
+
+
+const handler: NetworkEventHandler = async (socket, payload: string, player) => {
+	try {
+		const room = await getRoomById(player.roomId)
+		let [x, y, char] = payload
+		await editBaner(x, y, char, room)
+	}catch(error) {
+		sendEvent<string>(socket, ERROR_EVENT, error.message)
+		console.error(error)
+	}
+}
+
+networkEmitter.on(DRAW_EVENT, handler)
