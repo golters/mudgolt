c0029d29f417009917ba3e16a8a6b56ab70fde69
IamtheMa <83910227+IamtheMa@users.noreply.github.com>
2021-08-01 11:01:08 +0100

    Add files via upload
    deletedoor event

---
+++ server/network/events/deleteDoor.ts
@@ -0,0 +1,45 @@
+import {
+  networkEmitter, NetworkEventHandler,
+} from "./emitter"
+import {
+  ERROR_EVENT,
+  SERVER_LOG_EVENT,
+  DELETE_DOOR_EVENT,
+} from "../../../events"
+import {
+  sendEvent,
+} from "../../network"
+import {
+  createDoor,
+  getDoorByName,
+  deleteDoor,
+} from "../../services/door"
+import {
+  getRoomByName,
+} from "../../services/room"
+import { Player } from "@types"
+
+const handler: NetworkEventHandler = async (socket, args: string, player) => {
+  try {
+	  let [name] = args
+	  const door = await getDoorByName(player?.roomId, name)
+	  if (!door) {
+		  throw new Error("There is no such door")
+		  
+      return
+	  }
+
+	  const roomID = player.roomId
+	  name = name.replace(/\s/g, "_")
+	  await deleteDoor(roomID, name)
+
+
+	  sendEvent<string>(socket, SERVER_LOG_EVENT, `Deleted door ${name}`)
+  } catch (error) {
+    sendEvent<string>(socket, ERROR_EVENT, error.message)
+    console.error(error)
+  }
+}
+
+
+networkEmitter.on(DELETE_DOOR_EVENT, handler)
