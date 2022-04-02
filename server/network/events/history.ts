7a4fea3b318ee4d7e164e1425285d570b8bd4c0a
IamtheMa <adammagames@gmail.com>
2022-04-02 13:04:00 +0100

    /history command

---
+++ server/network/events/history.ts
@@ -0,0 +1,38 @@
+import {
+  networkEmitter, NetworkEventHandler, 
+} from "./emitter"
+import {
+  sendEvent,
+} from "../"
+import {
+  CHAT_HISTORY_EVENT, 
+  ERROR_EVENT,
+  HISTORY_EVENT,
+} from "../../../events"
+import {
+} from "../../../constants"
+import {
+  fetchRoomChats,
+} from "../../services/chat"
+import { Chat } from "../../../@types"
+
+const handler: NetworkEventHandler = async (socket, amount: number, player) => {
+  try {
+    const chats = await fetchRoomChats(player.roomId)
+    let loglength = 0
+    if(amount > chats.length){
+      sendEvent<string>(socket, ERROR_EVENT, `there are only ${chats.length} messages in this room`)
+    }else{
+      loglength = chats.length - amount
+    }
+    const log = chats.splice(loglength)
+
+    sendEvent<Chat[]>(socket, CHAT_HISTORY_EVENT, log)
+    
+  } catch (error) {
+    sendEvent<string>(socket, ERROR_EVENT, error.message)
+    console.error(error)
+  }
+}
+
+networkEmitter.on(HISTORY_EVENT, handler)
