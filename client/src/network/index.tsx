28623cd476b0f29529374a83ccf2c3972d350f14
IamtheMa <adammagames@gmail.com>
2022-04-15 07:21:25 +0100

    fix look on load issue

--- client/src/network/index.tsx
+++ client/src/network/index.tsx
@@ -79,7 +79,6 @@ export const networkTask = () => new Promise<void>((resolve) => {
       } else {
         sendEvent<null>(CHAT_HISTORY_EVENT, null)
         requestedChat = true
-        sendEvent(LOOK_EVENT, null)
       }
     }
 
@@ -92,6 +91,7 @@ export const networkTask = () => new Promise<void>((resolve) => {
         networkEmitter.emit(COMMAND_LOG_EVENT, chats[i])
         }
       }
+      sendEvent(LOOK_EVENT, null)
       resolve()
     }
     
