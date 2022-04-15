0059fec5d2d6c20683cd38923a9407e1c2b184f6
IamtheMa <adammagames@gmail.com>
2022-04-15 07:16:35 +0100

    disconnect messages

--- client/src/network/index.tsx
+++ client/src/network/index.tsx
@@ -56,7 +56,7 @@ export const networkTask = () => new Promise<void>((resolve) => {
   reconnectAttempts++
 
   client.addEventListener("open", () => {
-    //pushToLog("Connected to server")
+    pushToLog("Connected to server")
 
     reconnectAttempts = 0
   })
@@ -105,7 +105,7 @@ export const networkTask = () => new Promise<void>((resolve) => {
   })
 
   client.addEventListener("close", () => {
-    //pushErrorToLog(`Disconnected from server. Reconnecting...`)
+    pushErrorToLog(`Disconnected from server. Reconnecting...`)
 
     if (reconnectAttempts === 0) {
       networkTask().catch(console.error)
